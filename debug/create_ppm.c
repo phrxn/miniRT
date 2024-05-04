e476ed42392a7c7268be1bad428dfe064826edbe
gacalaza <gacalaza@42sp.org.br>
2024-05-04 15:55:35 -0300

    debug: add new chapter 5 debug

---
+++ debug/create_ppm.c
@@ -0,0 +1,49 @@
+#include "canvas.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static void    save_one_color(FILE *file_ppm, t_canvas *canvas, int color)
+{
+	int r;
+	int g;
+	int b; 
+	if (canvas->is_endian)
+	{
+		r = ((color >> 16) & 0xFF); //r
+		g = ((color >>  8) & 0xFF); //g
+		b = (color & 0xFF); //b
+	}
+	else
+	{
+		b = ((color >>  24) & 0xFF);
+		g = ((color >>  16) & 0xFF);
+		r = ((color >>  8) & 0xFF);
+	}
+	fprintf(file_ppm, "%d %d %d ", r, g, b);
+}
+
+void    save_ppm(t_canvas *canvas)
+{
+	FILE *file_ppm;
+	file_ppm = fopen("image.ppm", "w+");
+	if(!file_ppm)
+		printf("error when open image.ppm");
+
+	size_t count = 0;
+	size_t  colum_size = canvas->width - 1;
+	unsigned int canvas_size = canvas->height * canvas->width;
+	fprintf(file_ppm, "P3\n%d %d\n255\n", canvas->width, canvas->width);
+
+	for(unsigned int count = 0; count < canvas_size; count++)
+	{
+		save_one_color(file_ppm, canvas, canvas->pixels[count]);
+		if (count % canvas->width == colum_size)
+           fprintf(file_ppm, "\n");
+        else
+            fprintf(file_ppm, " ");
+	}
+
+	fclose(file_ppm);
+}
---
+++ debug/create_ppm.h
@@ -0,0 +1,8 @@
+#ifndef CREATE_PPM_H
+#define CREATE_PPM_H
+
+#include "canvas.h"
+
+void    save_ppm(t_canvas *canvas);
+
+#endif
