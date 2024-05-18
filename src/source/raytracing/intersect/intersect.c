4e78ecd06a1d860e54ca0d456ff3efa3f099a93e
phz <david@quazzom.com>
2024-05-18 19:12:59 -0300

    refactor: remove the ray transformation from sphere e put it in the intersect function

--- src/source/raytracing/intersect/intersect.c
+++ src/source/raytracing/intersect/intersect.c
@@ -3,22 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   intersect.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gacalaza <gacalaza@student.42sp.org.br     +#+  +:+       +#+        */
+/*   By: dmanoel- <dmanoel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 20:07:24 by gacalaza          #+#    #+#             */
-/*   Updated: 2024/05/06 20:39:17 by gacalaza         ###   ########.fr       */
+/*   Updated: 2024/05/18 19:08:35 by dmanoel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "intersect.h"
 #include "intersect_sphere.h"
 #include "messages.h"
+#include "transform.h"
 
-t_list	*intersect(t_shape *shape, t_ray *ray_transformed)
+static	t_list	*select_intersect(t_shape *shape, t_ray *ray_transformed)
 {
 	if (shape->type == TYPE_SPHERE)
 		return (intersect_sphere(shape, ray_transformed));
 	else
 		show_error_method("intersect", MERR_INVALID_OBJ_TYPE);
 	return (NULL);
+}
+
+t_list	*intersect(t_shape *shape, t_ray *ray_transformed)
+{
+	ray_transformed = transform_ray(shape, ray_transformed);
+	if (!ray_transformed)
+		return (NULL);
+	return (select_intersect(shape, ray_transformed));
 }
--- src/source/raytracing/intersect/intersect_sphere.c
+++ src/source/raytracing/intersect/intersect_sphere.c
@@ -6,7 +6,7 @@
 /*   By: dmanoel- <dmanoel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 21:14:34 by gacalaza          #+#    #+#             */
-/*   Updated: 2024/05/12 02:11:32 by dmanoel-         ###   ########.fr       */
+/*   Updated: 2024/05/18 19:02:50 by dmanoel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,9 +116,6 @@ t_list	*intersect_sphere(t_shape *shape, t_ray *ray_transformed)
 	double	abc[3];
 	double	ts[2];
 
-	ray_transformed = transform_ray(shape, ray_transformed);
-	if (!ray_transformed)
-		return (NULL);
 	discriminant = calc_disc(ray_transformed, shape, &status, abc);
 	destroy_ray(&ray_transformed);
 	if (status)
