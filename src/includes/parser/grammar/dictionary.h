700c6de00bd38c8219fbebfee239379fd10ac7b0
phrxn <phz@example.com>
2024-05-23 14:57:55 -0300

    fix: remove the BOOL type

--- src/includes/parser/grammar/dictionary.h
+++ src/includes/parser/grammar/dictionary.h
@@ -6,7 +6,7 @@
 /*   By: dmanoel- <dmanoel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 14:59:35 by dmanoel-          #+#    #+#             */
-/*   Updated: 2024/05/15 17:59:43 by dmanoel-         ###   ########.fr       */
+/*   Updated: 2024/05/23 14:57:09 by dmanoel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@
 # include "types.h"
 
 //base
-BOOL dict_is_a_identifier(t_token *token);
-BOOL dict_is_a_pos_int(t_token *token);
-BOOL dict_is_a_neg_int(t_token *token);
-BOOL dict_is_a_pos_double(t_token *token);
-BOOL dict_is_a_neg_double(t_token *token);
-BOOL dict_is_a_separator_value(t_token *token);
-BOOL dict_is_a_separator_property(t_token *token);
-BOOL dict_is_a_eol(t_token *token);
+char dict_is_a_identifier(t_token *token);
+char dict_is_a_pos_int(t_token *token);
+char dict_is_a_neg_int(t_token *token);
+char dict_is_a_pos_double(t_token *token);
+char dict_is_a_neg_double(t_token *token);
+char dict_is_a_separator_value(t_token *token);
+char dict_is_a_separator_property(t_token *token);
+char dict_is_a_eol(t_token *token);
 
 //derivation
 
-BOOL dict_is_a_number(t_token *token);
+char dict_is_a_number(t_token *token);
 
 #endif
--- src/source/parser/grammar/dictionary.c
+++ src/source/parser/grammar/dictionary.c
@@ -6,42 +6,42 @@
 /*   By: dmanoel- <dmanoel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 15:19:35 by dmanoel-          #+#    #+#             */
-/*   Updated: 2024/05/16 14:22:26 by dmanoel-         ###   ########.fr       */
+/*   Updated: 2024/05/23 14:57:09 by dmanoel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dictionary.h"
 #include "token.h"
 
-BOOL dict_is_a_identifier(t_token *token)
+char dict_is_a_identifier(t_token *token)
 {
 	if (token->type == identifier)
 		return (TRUE);
 	return (FALSE);
 }
 
-BOOL dict_is_a_pos_int(t_token *token)
+char dict_is_a_pos_int(t_token *token)
 {
 	if (token->type == pos_int)
 		return (TRUE);
 	return (FALSE);
 }
 
-BOOL dict_is_a_neg_int(t_token *token)
+char dict_is_a_neg_int(t_token *token)
 {
 	if (token->type == neg_int)
 		return (TRUE);
 	return (FALSE);
 }
 
-BOOL dict_is_a_pos_double(t_token *token)
+char dict_is_a_pos_double(t_token *token)
 {
 	if (token->type == pos_double)
 		return (TRUE);
 	return (FALSE);
 }
 
-BOOL dict_is_a_neg_double(t_token *token)
+char dict_is_a_neg_double(t_token *token)
 {
 	if (token->type == neg_double)
 		return (TRUE);
